ff2a7d2f6136fe19b2eedea71902e3db119feab2
Ali Malik <91459530+AliMalik9@users.noreply.github.com>
2025-06-04 03:02:16 +0500

    chat ui

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../supabaseClient";
+import Sidebar from "../components/Sidebar";
+import Receiver from "../components/Receiver";
+import Sender from "../components/Sender";
 
 function Chat() {
   const [user, setUser] = useState(null);
@@ -14,7 +17,7 @@ function Chat() {
       } = await supabase.auth.getSession();
 
       if (!session) {
-        navigate("/"); 
+        navigate("/");
       } else {
         setUser(session.user);
       }
@@ -33,13 +36,24 @@ function Chat() {
   if (loading) return <p>Loading...</p>;
 
   return (
-    <div>
-      <h1>Ham honge kamyab aik din</h1>
-      <h2>mazak kar raha hu</h2>
-      <p style={{ color: "#aaa" }}>
-        Logged in as: {user?.email}
-      </p>
-      <button
+    <div className="w-full h-screen bg-[#1e1e20] flex gap-5 p-5 overflow-hidden relative">
+      <div className="absolute w-[446px] h-[446px] rounded-full opacity-60 bg-[#adaeb3] blur-[250px] right-[-232px] top-[-232px]"></div>
+      <div className="absolute w-[446px] h-[446px] rounded-full opacity-60 bg-[#adaeb3] blur-[250px] left-[-232px] bottom-[-232px]"></div>
+      <Sidebar />
+      <div className="flex-1 h-full">
+      <div className="flex items-center gap-5 self-stretch">
+        <Receiver />
+        <Sender 
+          email={user?.email}
+          name={user?.user_metadata?.full_name || user?.email?.split('@')[0]}
+          image={user?.user_metadata?.avatar_url || user?.user_metadata?.picture} // Google OAuth provides 'picture', some other providers use 'avatar_url'
+        />
+      </div>
+      <div></div>
+      </div>
+
+      {/* <p style={{ color: "#aaa" }}>Logged in as: {user?.email}</p> */}
+      {/* <button
         onClick={handleLogout}
         style={{
           marginTop: "20px",
@@ -52,7 +66,7 @@ function Chat() {
         }}
       >
         Logout
-      </button>
+      </button> */}
     </div>
   );
 }
